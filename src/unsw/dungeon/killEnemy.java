f31b67ac43de84557c3a16f263001da6b30cd091
Your Name <github_email@example.com>
2019-10-31 18:12:03 +1100

    Basic gameplay logic, observer and composite patterns done

---
+++ src/unsw/dungeon/killEnemy.java
@@ -0,0 +1,28 @@
+package unsw.dungeon;
+
+public class killEnemy implements Goal {
+	
+	private String name = "enemy";
+	private Dungeon dungeon;
+
+	public killEnemy(Dungeon d) {
+		super();
+		this.dungeon = d;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public boolean accomplished() {
+		if (dungeon.getEnemyCount() == 0) {
+			return true;
+		}
+		return false;
+	}
+	
+}
