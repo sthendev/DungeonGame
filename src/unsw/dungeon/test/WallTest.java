ab7a5dbbaf6c6e9f000652a107a6c9bd9732447a
sthen <sanju.t2899@gmail.com>
2019-11-03 15:33:03 +1100

    Door test done

--- src/unsw/dungeon/test/WallTest.java
+++ src/unsw/dungeon/test/WallTest.java
@@ -4,24 +4,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import unsw.dungeon.Dungeon;
 import unsw.dungeon.Wall;
 import unsw.dungeon.Player;
 import unsw.dungeon.Enemy;
 import unsw.dungeon.Boulder;
-import unsw.dungeon.OffensiveEnemy;
 
 
 public class WallTest {
 	@Test
-	public void wallTest() {
-		Dungeon dungeon = new Dungeon(5, 5);
+	public void testWall() {
+		Wall wall = new Wall(null);
 		
-		Wall wall = new Wall(dungeon.getTile(2, 2));
-		
-		Player player = new Player(dungeon, dungeon.getTile(1, 2));
-		Enemy enemy =  new Enemy(dungeon, dungeon.getTile(1, 2), new OffensiveEnemy());
-		Boulder boulder = new Boulder(dungeon, dungeon.getTile(1, 2));
+		Player player = new Player(null, null);
+		Enemy enemy =  new Enemy(null, null, null);
+		Boulder boulder = new Boulder(null, null);
 		
 		// isBlocking
 		assertEquals(wall.isBlocking(player), true);
