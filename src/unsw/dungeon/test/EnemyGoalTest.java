4da5b1de8bbdde8bf3096a365526b48ad407f0e5
sthen <sanju.t2899@gmail.com>
2019-11-03 18:54:16 +1100

    all tests complete

--- src/unsw/dungeon/test/EnemyGoalTest.java
+++ src/unsw/dungeon/test/EnemyGoalTest.java
@@ -4,28 +4,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import unsw.dungeon.*;
 import org.junit.jupiter.api.Test;
 
-import unsw.dungeon.Dungeon;
-import unsw.dungeon.Player;
-
 public class EnemyGoalTest {
 	
 	@Test
 	public void swordKillEnemy() {
-		Dungeon dungeon = new Dungeon(5, 5);
+		Dungeon dungeon = new Dungeon(10, 10);
 		EnemiesGoal g= new EnemiesGoal();
 		dungeon.setGoal(g);
 		
 		Tile tile = dungeon.getTile(2, 2);
 		
-		Player player = new Player(null, null);
-		Enemy enemy = new Enemy(dungeon, null, null);
+		Player player = new Player(dungeon, dungeon.getTile(5, 5));
+		Enemy enemy = new Enemy(dungeon, dungeon.getTile(6, 5), null);
 		dungeon.addEnemy(enemy);
+		dungeon.addEntity(enemy);
 		tile.addEntity(enemy);
-		
+
+		assertEquals(dungeon.getGoal().satisfied(), false);
 		Sword sword = new Sword(null);
 		player.pickSword(sword);
-		
-		enemy.notifyComing(player);
+		player.Move(1, 0);
 		assertEquals(dungeon.getGoal().satisfied(), true);
 	}
 
